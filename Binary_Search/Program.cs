101f9c11f0156ec1ddc8b9eaa2bae903cb5b57f5
DESKTOP-H3RAS03\ivonn <ivonneture@gmail.com>
2026-04-07 00:28:13 +0800

    123

---
+++ Binary_Search/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Binary_Search
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int middle = left + (right - left) / 2;
+                if (target == nums[middle])
+                {
+                    return middle;
+                }
+                else if (target > nums[middle])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+
+            }
+            return -1;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            int[] intput1 = [-1, 0, 2, 4, 6, 8];
+            var result = solution.Search(intput1, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
