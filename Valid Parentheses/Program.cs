35f3a62fe67c263b6ec35249dd2d5c98cf89052a
DESKTOP-H3RAS03\ivonn <ivonneture@gmail.com>
2026-04-07 00:18:59 +0800

    123

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Valid_Parentheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            while (s.Contains("{}") || s.Contains("()") || s.Contains("[]")) 
+            {
+                s = s.Replace("{}","");
+                s = s.Replace("[]", "");
+                s = s.Replace("()", "");
+            }
+
+            return s == string.Empty;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new();
+            var input1 = "([{}])";
+            var result1 = solution.IsValid(input1);
+            Console.WriteLine($"Q1{result1}");
+
+            var input2 = "[(])";
+            var result2  = solution.IsValid(input2);
+            Console.WriteLine($"Q2{result2}");
+
+        }
+    }
+}
